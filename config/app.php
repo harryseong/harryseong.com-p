6c933bc428e90b70c3b11965ac8380036ec869b8
Harry Seong <harry@northwestern.edu>
2017-11-22 09:48:57 -0600

    Updates to Allow Production
    -Added 'Form' and 'Html' class aliases to the config file.
    -Updated AppServiceProvider.php boot() method to fix bug that prevented DB migrations.

--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         Illuminate\View\ViewServiceProvider::class,
         Mews\Purifier\PurifierServiceProvider::class,
         Intervention\Image\ImageServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -229,6 +230,8 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Purifier' => Mews\Purifier\Facades\Purifier::class,
         'Image' => Intervention\Image\Facades\Image::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
     ],
 
 ];
